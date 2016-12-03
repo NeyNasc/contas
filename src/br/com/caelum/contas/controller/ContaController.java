8e310939b11efa40eb2b6c2eb0a7c9d6d49fc393
NeyNasc <claudineydelima@gmail.com>
2016-12-03 20:43:26 -0200

    Listando contas com JSTL.

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -1,7 +1,10 @@
 package br.com.caelum.contas.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.contas.dao.ContaDAO;
 import br.com.caelum.contas.modelo.Conta;
@@ -9,12 +12,12 @@ import br.com.caelum.contas.modelo.Conta;
 @Controller
 public class ContaController {
 
-	@RequestMapping("form")
+	@RequestMapping("/form")
 	public String formulario(){
 		return "conta/formulario";
 	}
 	
-	@RequestMapping("adicionaConta")
+	@RequestMapping("/adicionaConta")
 	public String adiciona(Conta conta){
 
 		ContaDAO dao = new ContaDAO();
@@ -22,5 +25,15 @@ public class ContaController {
 		
 		
 		return "conta/conta-adicionada";
+	}
+	
+	@RequestMapping("listaContas")
+	public ModelAndView lista() {
+	  ContaDAO dao = new ContaDAO();
+	  List<Conta> contas = dao.lista();
+
+	  ModelAndView mv = new ModelAndView("conta/lista");
+	  mv.addObject("contas", contas);
+	  return mv;
 	}
 }
