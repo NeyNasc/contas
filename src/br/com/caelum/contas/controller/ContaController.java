b07cf27c4fc9014944f20e5ae4aae20212714409
NeyNasc <claudineydelima@gmail.com>
2016-12-10 14:18:49 -0200

    Injetando dependencias no projeto.

--- src/br/com/caelum/contas/controller/ContaController.java
+++ src/br/com/caelum/contas/controller/ContaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,45 +17,52 @@ import br.com.caelum.contas.modelo.Conta;
 @Controller
 public class ContaController {
 
+	private ContaDAO dao;
+
+	@Autowired
+	public ContaController(ContaDAO dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping("/form")
-	public String formulario(){
+	public String formulario() {
 		return "conta/formulario";
 	}
-	
+
 	@RequestMapping("/adicionaConta")
 	public String adiciona(@Valid Conta conta, BindingResult result) {
-	   
-	    if(result.hasErrors()) {
-	      return "conta/formulario";
-	    }
-		ContaDAO dao = new ContaDAO();
+
+		if (result.hasErrors()) {
+			return "conta/formulario";
+		}
+
 		dao.adiciona(conta);
-		
-		
+
 		return "redirect:listaContas";
 	}
-	
+
 	@RequestMapping("listaContas")
 	public ModelAndView lista() {
-	  ContaDAO dao = new ContaDAO();
-	  List<Conta> contas = dao.lista();
 
-	  ModelAndView mv = new ModelAndView("conta/lista");
-	  mv.addObject("contas", contas);
-	  return mv;
+		List<Conta> contas = dao.lista();
+
+		ModelAndView mv = new ModelAndView("conta/lista");
+		mv.addObject("contas", contas);
+		return mv;
 	}
-	
+
 	@RequestMapping("/removeConta")
-	public String remove(Conta conta){
-		ContaDAO dao = new ContaDAO();
+	public String remove(Conta conta) {
+
 		dao.remove(conta);
-		
+
 		return "redirect:listaContas";
 	}
+
 	@RequestMapping("/pagaConta")
 	public void paga(Long id, HttpServletResponse response) {
-	  ContaDAO dao = new ContaDAO();
-	  dao.paga(id);
-	  response.setStatus(200);
+
+		dao.paga(id);
+		response.setStatus(200);
 	}
 }
